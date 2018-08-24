dbcff5b93467d8bbeb1e522e739bdb28fd84ff14
AustinCreates154 <41809488+AustinCreates154@users.noreply.github.com>
2018-08-24 12:18:51 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require(`./config.json`)
+
+client.on('ready', () => {
+  console.log(`${client.user.tag}, is ready for action!`)
+  client.user.setActivity('Hello, there!')
+})
+
+client.on("message", message => {
+  if(author.user.id === "369256915479560192"){
+    if(message.content === "botdeveloper!shutdown"){
+      client.destroy(143)
+    }
+  }
+})
+
+client.on("message", message => {
+  if(author.user.id === "369256915479560192") {
+    if(message.content === "botdeveloper!ping")
+      message.channgel.send(`Pong, Ping is ${Math.floor(bot.ping)} ms`)
+  }
+})
+
+client.login(process.evl.TOKEN)
